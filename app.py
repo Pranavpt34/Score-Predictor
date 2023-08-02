f3f59b93d5c7aa0c815c428c26c7a06fe0405ef2
Pranav PT <pranavpt34@gmail.com>
2023-08-02 20:14:26 +0530

    This commit includes pickel file,csv file and json file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import pickle
 import numpy as np
 import pandas as pd
+import json
 
 
 app = Flask(__name__)
@@ -11,10 +12,99 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data = request.json['data']
-    print(data)
+df = pd.read_csv('df_average.csv')
+
+@app.route('/get_teams', methods=['GET'])
+def get_teams():
+    print("APP.py file")
+    teams = df['Team'].unique().tolist()
+    print(teams)
+    return jsonify({'teams': teams})
+
+@app.route('/get_players', methods=['POST'])
+def get_players():
+    data = request.json
+    selected_team = data['selected_team']
+    selected_players = df[df['Team'] == selected_team]['Player'].unique().tolist()
+    return jsonify({'players': selected_players})
+
+@app.route('/get_grounds', methods=['POST'])
+def get_grounds():
+    data = request.json
+
+    grounds = df['Ground'].unique().tolist()
+    return jsonify({'grounds': grounds})
+
+# grounds = df[(df['Team'] == selected_team) & (df['Player'] == selected_player)]['Ground'].unique().tolist()
+#submit player list
+@app.route('/submit_players', methods=['POST'])
+def submit_players():
+    data = request.get_json()
+    selected_players = data.get('selected_players', [])
+    print(selected_players)
+    response = {'message': 'Players received successfully.'}
+    return jsonify(response) 
+
+
+
+# @app.route('/submit_selection', methods=['POST'])
+# def submit_selection():
+#     data = request.json
+#     selected_team = data['selected_team']
+#     selected_player = data['selected_player']
+#     selected_ground = data['selected_ground']
+#     # Process the selected data as required
+#     print(f'Selected Team: {selected_team}')
+#     print(f'Selected Player: {selected_player}')
+#     print(f'Selected Ground: {selected_ground}')
+#     return jsonify({'message': 'Selection submitted successfully!'})
+
+# @app.route('/get_average_strikerate', methods=['POST'])
+# def get_average_strikerate():
+#     data = request.json
+#     player_name = data['player_name']
+#     ground = data['ground']
+#     opposition_team = data['opposition_team']
+
+#     # Filter the dataframe based on user input
+#     filtered_df = df[
+#         (df['Player'] == player_name) &
+#         (df['Ground'] == ground) &
+#         (df['Opposition'] == opposition_team)
+#     ]
+
+#     # Calculate the average strikerate for the filtered rows
+#     average_strikerate = filtered_df['SR'].mean()
+
+#     # Return the result as JSON
+#     return jsonify({'average_strikerate': average_strikerate})
+
+# with open('columns.json') as f:
+#         columns = json.load(f)
+#         Opposition = columns["data_columns"][152:167]
+#         Player = columns["data_columns"][2:151]
+#         Ground = columns["data_columns"][167:]
+
+
+# @app.route('/get_teams', methods=['GET'])
+# def get_teams():
+#     return jsonify({'teams': Opposition})
+
+
+
+# @app.route('/get_teams', methods=['GET'])
+# def get_teams():
+#     with open('columns.json') as f:
+#         teams = json.load(f)
+#         print("json file ",teams)
+#     return jsonify(teams) 
+
+@app.route('/select_team', methods=['POST'])
+def select_team():
+    data = request.get_json()
+    selected_team = data.get('team')
+    print(f'Selected team: {selected_team}')
+    return jsonify({'message': f'Team "{selected_team}" selected successfully!'})
 
 
 
--- static/index.js
+++ static/index.js
@@ -22,3 +22,226 @@ function hideAllLetters() {
 }
 
 showNextLetter(); // Start the sequence
+
+
+// function fetchTeams() {
+//     console.log("hai")
+//     fetch('/get_teams')
+//         .then(response => response.json())
+//         .then(data => {
+//             const teamDropdown = document.getElementById('team-dropdown');
+//             const teams = data.teams; // Extract the teams array from the JSON response
+//             teams.forEach(team => {
+//                 const option = document.createElement('option');
+//                 option.value = team;
+//                 option.textContent = team;
+//                 teamDropdown.appendChild(option);
+//             });
+//         })
+//         .catch(error => console.error('Error fetching teams:', error));
+// }
+
+// function selectTeam() {
+//     const selectedTeam = document.getElementById('team-dropdown').value;
+//     if (selectedTeam) {
+//         // Send the selected team to the Flask backend using AJAX
+//         fetch('/select_team', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ 'team': selectedTeam })
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data.message);
+//         })
+//         .catch(error => {
+//             console.error('Error selecting team:', error);
+//         });
+//     }
+// }
+
+// Fetch teams when the page loads
+
+function fetchTeams() {
+    fetch('/get_teams')
+        .then(response => response.json())
+        .then(data => {
+            console.log("Here it is",data)
+            const teamDropdown = document.getElementById('team-dropdown');
+            data.teams.forEach(team => {
+                const option = document.createElement('option');
+                option.value = team;
+                option.textContent = team;
+                teamDropdown.appendChild(option);
+            });
+        })
+        .catch(error => console.error('Error fetching teams:', error));
+}
+
+
+
+// old function
+// function fetchPlayers() {
+//     const selectedTeam = document.getElementById('team-dropdown').value;
+//     if (selectedTeam) {
+//         fetch('/get_players', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ 'selected_team': selectedTeam })
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log("data",data.players)
+//             // const playerDropdown = document.getElementById('player-dropdown');
+//             // playerDropdown.innerHTML = ''; // Clear previous options
+//             // data.players.forEach(player => {
+//             //     const option = document.createElement('option');
+//             //     option.value = player;
+//             //     option.textContent = player;
+//             //     playerDropdown.appendChild(option);
+//             // });
+//             // playerDropdown.disabled = false; // Enable player dropdown
+            
+//         })
+//         .catch(error => console.error('Error fetching players:', error));
+//     }
+// }
+// function createCheckboxes() {
+//     const checkboxContainer = document.getElementById("checkboxContainer");
+  
+//     data.forEach((item) => {
+//       const checkboxDiv = createCheckbox(item);
+//       checkboxContainer.appendChild(checkboxDiv);
+//     });
+//   }
+function fetchPlayers() {
+    const selectedTeam = document.getElementById('team-dropdown').value;
+    if (selectedTeam) {
+      fetch('/get_players', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 'selected_team': selectedTeam })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log("data", data.players)
+          const checkboxContainer = document.getElementById("checkboxContainer");
+          checkboxContainer.innerHTML = ''; // Clear previous checkboxes
+
+          data.players.forEach(player => {
+            const checkboxDiv = createCheckbox(player);
+            checkboxContainer.appendChild(checkboxDiv);
+          });
+        })
+        .catch(error => console.error('Error fetching players:', error));
+    }
+  }
+
+  // Function to create checkboxes
+  function createCheckbox(name) {
+    const checkboxDiv = document.createElement("div");
+    checkboxDiv.className = "form-check";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "form-check-input";
+    checkbox.name = "players"; // Change the name to 'players' for multiple checkboxes
+    checkbox.value = name;
+
+    const label = document.createElement("label");
+    label.className = "form-check-label";
+    label.textContent = name;
+
+    checkboxDiv.appendChild(checkbox);
+    checkboxDiv.appendChild(label);
+
+    return checkboxDiv;
+  }
+
+// submit form to player list to backend
+function submitPlayerSelection() {
+    const selectedPlayers = Array.from(document.querySelectorAll('input[name="players"]:checked')).map(checkbox => checkbox.value);
+    console.log("Here is data",selectedPlayers)
+    // Replace '/submit_players' with the actual endpoint URL in your Flask app
+    fetch('/submit_players', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 'selected_players': selectedPlayers })
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response from the Flask server if needed
+        console.log(data);
+      })
+      .catch(error => console.error('Error submitting players:', error));
+  }
+
+function fetchGrounds() {
+    const selectedTeam = document.getElementById('team-dropdown').value;
+    const selectedPlayer = document.getElementById('player-dropdown').value;
+    if (selectedTeam && selectedPlayer) {
+        fetch('/get_grounds', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'selected_team': selectedTeam,
+                'selected_player': selectedPlayer
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            const groundDropdown = document.getElementById('ground-dropdown');
+            groundDropdown.innerHTML = ''; // Clear previous options
+            data.grounds.forEach(ground => {
+                const option = document.createElement('option');
+                option.value = ground;
+                option.textContent = ground;
+                groundDropdown.appendChild(option);
+            });
+            groundDropdown.disabled = false; // Enable ground dropdown
+            document.getElementById('submit-button').disabled = false; // Enable submit button
+        })
+        .catch(error => console.error('Error fetching grounds:', error));
+    }
+}
+
+function enableSubmitButton() {
+    document.getElementById('submit-button').disabled = false;
+}
+
+function submitSelection() {
+    const selectedTeam = document.getElementById('team-dropdown').value;
+    const selectedPlayer = document.getElementById('player-dropdown').value;
+    const selectedGround = document.getElementById('ground-dropdown').value;
+
+    const requestData = {
+        'selected_team': selectedTeam,
+        'selected_player': selectedPlayer,
+        'selected_ground': selectedGround
+    };
+
+    fetch('/submit_selection', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+    .then(response => response.json)
+    .then(data => {
+        console.log(data.message);
+    })
+    .catch(error => console.error('Error submitting selection:', error));
+}
+
+fetchTeams(); 
